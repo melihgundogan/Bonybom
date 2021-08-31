e27a06b8af3344365efdcb4ae265cca62db05fb6
bersaaa <besra8533@gmail.com>
2021-08-31 15:44:46 +0300

    düzenleme

---
+++ lib/source/icons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class icons {
+  Widget home =
+      SvgPicture.asset('assets/icons/Home.svg', semanticsLabel: 'Home');
+  Widget category =
+      SvgPicture.asset('assets/icons/category.svg', semanticsLabel: 'Category');
+  Widget warning =
+      SvgPicture.asset('assets/icons/warning.svg', semanticsLabel: 'Warning');
+  Widget notification = SvgPicture.asset('assets/icons/notification.svg',
+      semanticsLabel: 'Notification');
+  Widget email =
+      SvgPicture.asset('assets/icons/email.svg', semanticsLabel: 'Email');
+  Widget heart =
+      SvgPicture.asset('assets/icons/heart.svg', semanticsLabel: 'Heart');
+}
