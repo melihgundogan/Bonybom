7667f0bc3aa597ad3f944551839bf4fdd5d48604
bersaaa <65489454+bersaaa@users.noreply.github.com>
2021-09-10 23:35:41 +0300

    info

--- lib/pages/auth/info_pages/countries.dart
+++ lib/pages/auth/info_pages/countries.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:bonybom_app/pages/auth/info_pages/answer_countries.dart';
+import 'package:bonybom_app/pages/auth/info_pages/getting_countries.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class Countries extends StatefulWidget {
   @override
@@ -9,12 +11,23 @@ class Countries extends StatefulWidget {
 }
 
 class _CountriesState extends State<Countries> {
-
+  List<GettingCountries> parseAnswerCountry(String answer){
+    return AnswerCountries.fromJson(json.decode(answer)).CountriesList;
+  }
   
+  Future <List<GettingCountries>> searchCountry() async{
+    var url = Uri.parse("https://bonybom.com/api/location/countries/all");
+    var answer = await http.get(url);
+    return parseAnswerCountry(answer.body);
+  }
+  
+
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      
+    );
     
  }
 }
