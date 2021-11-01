975211f2e6583c42656817c70023315c697e20df
mserif <mehmetserif@gmail.com>
2021-11-01 19:36:50 +0300

    Fix-import on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-=======
-import 'package:bonybom_app/dark_light.dart';
-import 'package:bonybom_app/opening.dart';
-import 'package:bonybom_app/screens/category/category_select.dart';
-import 'package:bonybom_app/screens/category/other_categorys.dart';
-import 'package:bonybom_app/screens/category/special_category.dart';
-import 'package:bonybom_app/screens/category/to_you.dart';
-import 'package:bonybom_app/screens/home/home_page.dart';
-import 'package:bonybom_app/screens/more_diamond/diamond.dart';
-import 'package:bonybom_app/screens/more_diamond/moreoptions.dart';
-import 'package:bonybom_app/screens/notification/notification.dart';
-import 'package:bonybom_app/screens/profile/clean_up_cache.dart';
-import 'package:bonybom_app/screens/profile/country_choose.dart';
-import 'package:bonybom_app/screens/profile/editprofile.dart';
-import 'package:bonybom_app/screens/profile/info.dart';
-import 'package:bonybom_app/source/providers.dart';
-import 'package:bonybom_app/source/response_notifier.dart';
->>>>>>> d8d12d8ad7d2a01b719a2c331a9297b095ee1e57
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
