3df4dd16eaeee20344f56b41a70f90918b096bc9
mserif <mehmetserif@gmail.com>
2021-11-01 19:33:53 +0300

    Basic domain layer has finished

--- lib/domain/usecases/suggestion/get_single_suggestion.dart
+++ lib/domain/usecases/suggestion/get_single_suggestion.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
 
-import '../../core/error/failures.dart';
-import '../../core/usecases/usecase.dart';
-import '../entities/suggestion.dart';
-import '../params/suggestion_params.dart';
-import '../repositories/suggestion_repository.dart';
+import '../../../core/error/failures.dart';
+import '../../../core/usecases/usecase.dart';
+import '../../entities/suggestion.dart';
+import '../../params/suggestion_params.dart';
+import '../../repositories/suggestion_repository.dart';
 
 class GetSingleSuggestion implements UseCase<Suggestion, SuggestionParams> {
   final SuggestionRepository suggestionRepository;
