db1ce53e4f417b807cb76ff2c96e50c2a624afd6
Muhammed Melih GÜNDOĞAN <muhammedmelihgundogan@gmail.com>
2021-12-07 01:10:21 +0300

    eng.json and tr.json files are prepared

--- lib/core/init/lang/locale_keys.g.dart
+++ lib/core/init/lang/locale_keys.g.dart
@@ -1,6 +1,44 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
 abstract class  LocaleKeys {
+  static const opening_text = 'opening_text';
+  static const let_is_start = 'let_is_start';
+  static const create_new_account = 'create_new_account';
+  static const fill_out_form = 'fill_out_form';
+  static const name_surname = 'name_surname';
+  static const register = 'register';
+  static const do_you_have_an_account = 'do_you_have_an_account';
   static const login = 'login';
+  static const welcome_again = 'welcome_again';
+  static const please_enter_your_information = 'please_enter_your_information';
+  static const password = 'password';
+  static const forgot_my_password = 'forgot_my_password';
+  static const continue_with_google = 'continue_with_google';
+  static const register_now = 'register_now';
+  static const do_not_have_an_account = 'do_not_have_an_account';
+  static const date_of_birth = 'date_of_birth';
+  static const choose_city = 'choose_city';
+  static const gender = 'gender';
+  static const information_text = 'information_text';
+  static const setting = 'setting';
+  static const edit_profile = 'edit_profile';
+  static const notifications = 'notifications';
+  static const edit_categories = 'edit_categories';
+  static const dark_mode = 'dark_mode';
+  static const share_bonybom_friend = 'share_bonybom_friend';
+  static const contact_us = 'contact_us';
+  static const about_bonybom = 'about_bonybom';
+  static const privacy_policy = 'privacy_policy';
+  static const terms_of_use = 'terms_of_use';
+  static const my_account = 'my_account';
+  static const phone_number = 'phone_number';
+  static const e_mail = 'e_mail';
+  static const city = 'city';
+  static const account_transactions = 'account_transactions';
+  static const delete_account = 'delete_account';
+  static const sign_out = 'sign_out';
+  static const saved = 'saved';
+  static const favorites = 'favorites';
+  static const category_choose_information = 'category_choose_information';
 
 }
